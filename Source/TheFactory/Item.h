7cf9d0d3f46ddb51d44c473647a0c1939c05b2f8
Sonwonill <example@aa.com>
2024-05-22 18:53:03 +0900

    :sparkles: Feat : "상호작용 구현 작업  시작"
    Interactable 인터페이스 생성 및 부모 클래스로 사용할 Item cpp 클래스 생성

---
+++ Source/TheFactory/Item.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interactable.h"
+#include "Item.generated.h"
+
+UCLASS()
+class THEFACTORY_API AItem : public AActor, public IInteractable
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItem();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Info")
+	void itemKey;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Interactable 추상 메서드
+	virtual void StartInteract() override;
+	virtual void EndInteract() override;
+
+	UFUNCTION()
+	void Delete();
+	UFUNCTION()
+	void Use();
+	UFUNCTION()
+	void Picked();
+	UFUNCTION()
+	void Put()
+
+};
